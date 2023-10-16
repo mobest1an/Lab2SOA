aecb9fa1116466b08c86a4979bd05ec338ddc3db
mobest1an <erikk202@gmail.com>
2023-10-16 03:53:21 +0300

    Cleanup code (NOT TESTED!!!)

--- services/flat-service/src/main/kotlin/com/iver/flatservice/service/FlatSpecification.kt
+++ services/flat-service/src/main/kotlin/com/iver/flatservice/service/FlatSpecification.kt
@@ -7,6 +7,44 @@ import org.springframework.data.jpa.domain.Specification
 import java.util.*
 import javax.persistence.criteria.*
 
+private fun <T : Enum<T>> getInstance(value: String, enumClass: Class<T>): T {
+    return java.lang.Enum.valueOf(enumClass, value)
+}
+
+private fun <T : Enum<T>> getEnumPredicates(
+    criteriaBuilder: CriteriaBuilder,
+    root: Root<Flat>,
+    key: String,
+    value: String,
+    enumClass: Class<T>,
+): Array<Predicate> {
+    val predicates = mutableListOf<Predicate>()
+    predicates.add(criteriaBuilder.equal(root.get<Any>(key), getInstance(value, enumClass)))
+    predicates.add(criteriaBuilder.notEqual(root.get<Any>(key), getInstance(value, enumClass)))
+    predicates.add(criteriaBuilder.greaterThan(root.get(key), getInstance(value, enumClass)))
+    predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get(key), getInstance(value, enumClass)))
+    predicates.add(criteriaBuilder.lessThan(root.get(key), getInstance(value, enumClass)))
+    predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get(key), getInstance(value, enumClass)))
+
+    return predicates.toTypedArray()
+}
+
+private fun <T : Comparable<T>> getCommonPredicates(
+    path: Expression<out T>,
+    criteriaBuilder: CriteriaBuilder,
+    value: T,
+): Array<Predicate> {
+    val predicates = mutableListOf<Predicate>()
+
+    predicates.add(criteriaBuilder.equal(path, value))
+    predicates.add(criteriaBuilder.notEqual(path, value))
+    predicates.add(criteriaBuilder.greaterThan(path, value))
+    predicates.add(criteriaBuilder.greaterThanOrEqualTo(path, value))
+    predicates.add(criteriaBuilder.lessThan(path, value))
+    predicates.add(criteriaBuilder.lessThanOrEqualTo(path, value))
+
+    return predicates.toTypedArray()
+}
 
 class FlatSpecification(private val searchCriteria: SearchCriteria) : Specification<Flat> {
 
@@ -15,58 +53,35 @@ class FlatSpecification(private val searchCriteria: SearchCriteria) : Specificat
         val baseKey = searchCriteria.key.split(".")[0]
 
         val predicates = mutableListOf<Predicate>()
+
+
         when (root.get<Any>(baseKey).javaType) {
             Furnish.FINE.javaClass -> {
-                predicates.add(criteriaBuilder.equal(root.get<Any>(searchCriteria.key), Furnish.valueOf(strToSearch)))
-                predicates.add(criteriaBuilder.notEqual(root.get<Any>(searchCriteria.key), Furnish.valueOf(strToSearch)))
-                predicates.add(criteriaBuilder.greaterThan(root.get(searchCriteria.key), Furnish.valueOf(strToSearch)))
-                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get(searchCriteria.key), Furnish.valueOf(strToSearch)))
-                predicates.add(criteriaBuilder.lessThan(root.get(searchCriteria.key), Furnish.valueOf(strToSearch)))
-                predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get(searchCriteria.key), Furnish.valueOf(strToSearch)))
+                predicates += getEnumPredicates(criteriaBuilder, root, baseKey, strToSearch, Furnish.FINE.javaClass)
             }
+
             Transport.ENOUGH.javaClass -> {
-                predicates.add(criteriaBuilder.equal(root.get<Any>(searchCriteria.key), Transport.valueOf(strToSearch)))
-                predicates.add(criteriaBuilder.notEqual(root.get<Any>(searchCriteria.key), Transport.valueOf(strToSearch)))
-                predicates.add(criteriaBuilder.greaterThan(root.get(searchCriteria.key), Transport.valueOf(strToSearch)))
-                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get(searchCriteria.key), Transport.valueOf(strToSearch)))
-                predicates.add(criteriaBuilder.lessThan(root.get(searchCriteria.key), Transport.valueOf(strToSearch)))
-                predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get(searchCriteria.key), Transport.valueOf(strToSearch)))
+                predicates += getEnumPredicates(criteriaBuilder, root, baseKey, strToSearch, Transport.ENOUGH.javaClass)
             }
+
             View.NORMAL.javaClass -> {
-                predicates.add(criteriaBuilder.equal(root.get<Any>(searchCriteria.key), View.valueOf(strToSearch)))
-                predicates.add(criteriaBuilder.notEqual(root.get<Any>(searchCriteria.key), View.valueOf(strToSearch)))
-                predicates.add(criteriaBuilder.greaterThan(root.get(searchCriteria.key), View.valueOf(strToSearch)))
-                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get(searchCriteria.key), View.valueOf(strToSearch)))
-                predicates.add(criteriaBuilder.lessThan(root.get(searchCriteria.key), View.valueOf(strToSearch)))
-                predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get(searchCriteria.key), View.valueOf(strToSearch)))
+                predicates += getEnumPredicates(criteriaBuilder, root, baseKey, strToSearch, View.NORMAL.javaClass)
             }
+
             House::class.java -> {
                 val houseJoin = root.join<Flat, House>("house")
                 val nestedKey = searchCriteria.key.split(".")[1]
-                predicates.add(criteriaBuilder.equal(houseJoin.get<Any>(nestedKey), strToSearch))
-                predicates.add(criteriaBuilder.notEqual(houseJoin.get<Any>(nestedKey), strToSearch))
-                predicates.add(criteriaBuilder.greaterThan(houseJoin.get(nestedKey), strToSearch))
-                predicates.add(criteriaBuilder.greaterThanOrEqualTo(houseJoin.get(nestedKey), strToSearch))
-                predicates.add(criteriaBuilder.lessThan(houseJoin.get(nestedKey), strToSearch))
-                predicates.add(criteriaBuilder.lessThanOrEqualTo(houseJoin.get(nestedKey), strToSearch))
+                predicates += getCommonPredicates(houseJoin.get(nestedKey), criteriaBuilder, strToSearch)
             }
+
             Coordinates::class.java -> {
                 val coordinatesJoin = root.join<Flat, Coordinates>("coordinates")
                 val nestedKey = searchCriteria.key.split(".")[1]
-                predicates.add(criteriaBuilder.equal(coordinatesJoin.get<Any>(nestedKey), strToSearch))
-                predicates.add(criteriaBuilder.notEqual(coordinatesJoin.get<Any>(nestedKey), strToSearch))
-                predicates.add(criteriaBuilder.greaterThan(coordinatesJoin.get(nestedKey), strToSearch))
-                predicates.add(criteriaBuilder.greaterThanOrEqualTo(coordinatesJoin.get(nestedKey), strToSearch))
-                predicates.add(criteriaBuilder.lessThan(coordinatesJoin.get(nestedKey), strToSearch))
-                predicates.add(criteriaBuilder.lessThanOrEqualTo(coordinatesJoin.get(nestedKey), strToSearch))
+                predicates += getCommonPredicates(coordinatesJoin.get(nestedKey), criteriaBuilder, strToSearch)
             }
+
             else -> {
-                predicates.add(criteriaBuilder.equal(root.get<Any>(searchCriteria.key), strToSearch))
-                predicates.add(criteriaBuilder.notEqual(root.get<Any>(searchCriteria.key), strToSearch))
-                predicates.add(criteriaBuilder.greaterThan(root.get(searchCriteria.key), strToSearch))
-                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get(searchCriteria.key), strToSearch))
-                predicates.add(criteriaBuilder.lessThan(root.get(searchCriteria.key), strToSearch))
-                predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get(searchCriteria.key), strToSearch))
+                predicates += getCommonPredicates(root.get(baseKey), criteriaBuilder, strToSearch)
             }
         }
 
@@ -78,21 +93,27 @@ class FlatSpecification(private val searchCriteria: SearchCriteria) : Specificat
             SearchOperation.EQUAL -> {
                 predicates[0]
             }
+
             SearchOperation.NOT_EQUAL -> {
                 predicates[1]
             }
+
             SearchOperation.GREATER_THAN -> {
                 predicates[2]
             }
+
             SearchOperation.GREATER_THAN_EQUAL -> {
                 predicates[3]
             }
+
             SearchOperation.LESS_THAN -> {
                 predicates[4]
             }
+
             SearchOperation.LESS_THAN_EQUAL -> {
                 predicates[5]
             }
+
             else -> {
                 null
             }
