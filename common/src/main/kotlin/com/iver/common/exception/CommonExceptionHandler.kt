be20f158b38429ace952de6befd263fdad1ac24d
erikk <erikk202@gmail.com>
2023-10-28 17:22:09 +0300

    Add exception handlers

--- common/src/main/kotlin/com/iver/common/exception/CommonExceptionHandler.kt
+++ common/src/main/kotlin/com/iver/common/exception/CommonExceptionHandler.kt
@@ -1,11 +1,15 @@
 package com.iver.common.exception
 
 import org.springframework.core.annotation.Order
+import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.data.mapping.PropertyReferenceException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 
 
 @ControllerAdvice
@@ -31,6 +35,72 @@ class CommonExceptionHandler {
                 ErrorResponseWrapper(
                     status = HTTPErrorResponseStatus.BAD_REQUEST,
                     description = e.message
+                )
+            )
+
+    @ExceptionHandler
+    fun handlePropertyReference(e: PropertyReferenceException): ResponseEntity<ErrorResponseWrapper> =
+        ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(
+                ErrorResponseWrapper(
+                    status = HTTPErrorResponseStatus.BAD_REQUEST,
+                    description = e.message
+                )
+            )
+
+    @ExceptionHandler
+    fun handleIllegalArgument(e: IllegalArgumentException): ResponseEntity<ErrorResponseWrapper> =
+        ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(
+                ErrorResponseWrapper(
+                    status = HTTPErrorResponseStatus.BAD_REQUEST,
+                    description = e.message ?: "Request parameters not correct"
+                )
+            )
+
+    @ExceptionHandler
+    fun handleIndexOutOfBounds(e: IndexOutOfBoundsException): ResponseEntity<ErrorResponseWrapper> =
+        ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(
+                ErrorResponseWrapper(
+                    status = HTTPErrorResponseStatus.BAD_REQUEST,
+                    description = e.message ?: "Request parameters not correct"
+                )
+            )
+
+    @ExceptionHandler
+    fun handleEmptyResultDataAccess(e: EmptyResultDataAccessException): ResponseEntity<ErrorResponseWrapper> =
+        ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(
+                ErrorResponseWrapper(
+                    status = HTTPErrorResponseStatus.BAD_REQUEST,
+                    description = e.message ?: "Request parameters not correct"
+                )
+            )
+
+    @ExceptionHandler
+    fun handleMethodArgumentTypeMismatch(e: MethodArgumentTypeMismatchException): ResponseEntity<ErrorResponseWrapper> =
+        ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(
+                ErrorResponseWrapper(
+                    status = HTTPErrorResponseStatus.BAD_REQUEST,
+                    description = e.message ?: "Request parameters not correct"
+                )
+            )
+
+    @ExceptionHandler
+    fun handleMessageNotReadable(e: HttpMessageNotReadableException): ResponseEntity<ErrorResponseWrapper> =
+        ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(
+                ErrorResponseWrapper(
+                    status = HTTPErrorResponseStatus.BAD_REQUEST,
+                    description = e.message ?: "Request parameters not correct"
                 )
             )
 }
