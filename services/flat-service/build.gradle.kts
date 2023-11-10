26999133283520ab9edd50338dfe50e9c8c34d99
Ivan Kustarev <ivan.kustarev@tune-it.ru>
2023-11-11 00:13:55 +0300

    fix https

--- services/flat-service/build.gradle.kts
+++ services/flat-service/build.gradle.kts
@@ -37,6 +37,15 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
+configurations {
+    all {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+        exclude(group = "ch.qos.logback", module = "logback-classic")
+        exclude(group = "org.apache.logging.log4j", module = "log4j-to-slf4j")
+    }
+}
+
+
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
--- services/flat-service/src/main/kotlin/com/iver/flatservice/FlatServiceApplication.kt
+++ services/flat-service/src/main/kotlin/com/iver/flatservice/FlatServiceApplication.kt
@@ -3,7 +3,9 @@ package com.iver.flatservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
@@ -13,7 +15,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 @EntityScan(basePackages = ["com.iver.common.model", "com.iver.flatservice"])
 @EnableJpaRepositories(basePackages = ["com.iver.common", "com.iver.flatservice"])
 @ComponentScan(basePackages = ["com.iver.common", "com.iver.flatservice"])
-class FlatServiceApplication
+class FlatServiceApplication: SpringBootServletInitializer() {
+
+    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
+        return builder.sources(FlatServiceApplication::class.java)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<FlatServiceApplication>(*args)
