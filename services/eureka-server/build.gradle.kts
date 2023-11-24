691e4f62936a675126b9abbf665cfe76f651c720
Ivan Kustarev <ivan.kustarev@tune-it.ru>
2023-11-24 14:54:28 +0300

    add eureka

--- services/eureka-server/build.gradle.kts
+++ services/eureka-server/build.gradle.kts
@@ -1,4 +1,3 @@
-import org.gradle.internal.Actions.set
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -21,22 +20,12 @@ repositories {
 }
 
 dependencies {
-    implementation(project(":common"))
-
     implementation("org.springframework.boot:spring-boot-starter-web")
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
-
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    implementation("org.springframework.cloud:spring-cloud-starter-config")
-    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-//    implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
 }
 
 tasks.withType<KotlinCompile> {
@@ -51,8 +40,10 @@ tasks.withType<Test> {
 }
 
 tasks.bootJar {
-    archiveFileName.set("agency-server.jar")
+    archiveFileName.set("eureka-server.jar")
 }
+
+tasks.register("prepareKotlinBuildScriptModel")
 
 dependencyManagement {
     imports {
