489dd60db867c64323eceac43b7d650188490af6
Ivan Kustarev <ivan.kustarev@tune-it.ru>
2023-12-23 01:49:03 +0300

    add soap

--- services/agency-service/src/main/kotlin/com/iver/agencyservice/api/v1/http/soap/AgencyServiceEndpoint.kt
+++
@@ -1,6 +0,0 @@
-package com.iver.agencyservice.api.v1.http.soap;
-
-public interface AgencyServiceEndpoint {
-//    fun getCheapestFlat(id1: Long, id2: Long): FlatView
-    fun getFlatsTotalCost(): Double
-}
