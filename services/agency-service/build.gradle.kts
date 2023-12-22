7abca26fff539815d5dff6dabbbdc9bacc9213f4
Ivan Kustarev <ivan.kustarev@tune-it.ru>
2023-12-22 22:36:23 +0300

    old changes

--- services/agency-service/build.gradle.kts
+++ services/agency-service/build.gradle.kts
@@ -20,6 +20,8 @@ repositories {
     mavenCentral()
 }
 
+val jaxb by configurations.creating
+
 dependencies {
     implementation(project(":common"))
 
@@ -36,8 +38,71 @@ dependencies {
 
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+
+    implementation("org.springframework.boot:spring-boot-starter-web-services")
+    implementation("wsdl4j:wsdl4j")
+
+    implementation("org.reactivestreams:reactive-streams:1.0.1")
+    implementation("javax.activation:activation:1.1.1")
+    implementation("javax.xml.bind:jaxb-api:2.4.0-b180830.0359")
+    implementation("org.glassfish.jaxb:jaxb-runtime")
+
+    jaxb("com.sun.xml.bind:jaxb-xjc:2.1.7")
+//    implementation(files(genJaxb.classesDir).builtBy(genJaxb))
+
+
 //    implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
 }
+
+configurations {
+    jaxb
+}
+
+tasks.register("genJaxb") {
+    ext["sourcesDir"] = "${buildDir}/generated-sources/jaxb"
+    ext["classesDir"] = "${buildDir}/classes/jaxb"
+    ext["schema"] = "C:\\Users\\Ivan\\IdeaProjects\\Lab2SOA\\services\\agency-service\\src\\main\\resources\\agency.xsd"
+
+    ext["classesDir"]?.let { outputs.dir(it) }
+
+    doLast {
+        ant.withGroovyBuilder {
+            "taskdef"(
+                "name" to "xjc", "classname" to "com.sun.tools.xjc.XJCTask",
+                "classpath" to jaxb.asPath
+            )
+            ext["sourcesDir"]?.let { mkdir(it) }
+            ext["classesDir"]?.let { mkdir(it) }
+
+            "xjc"(
+                "destdir" to ext["sourcesDir"], "schema" to ext["schema"]
+//                "package" to "target.package.name"
+            ) {
+                "arg"("value" to "-wsdl")
+                "produces"("dir" to ext["sourcesDir"], "includes" to "**/*.java")
+            }
+
+            "javac"(
+                "destdir" to ext["classesDir"], "source" to 1.8, "target" to 1.8, "debug" to true,
+                "debugLevel" to "lines,vars,source", "classpath" to jaxb.asPath
+            ) {
+                "src"("path" to ext["sourcesDir"])
+                "include"("name" to "**/*.java")
+                "include"("name" to "*.java")
+            }
+
+            "copy"("todir" to ext["classesDir"]) {
+                "fileset"("dir" to ext["sourcesDir"], "erroronmissingdir" to false) {
+                    "exclude"("name" to "**/*.java")
+                }
+            }
+        }
+    }
+}
+tasks.build {
+    dependsOn("genJaxb")
+}
+
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
